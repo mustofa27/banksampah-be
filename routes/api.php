9156588f6e50aaad875f0e59f39f9e66f3d42168
Ahmad Mustofa <mustofaahmad327@gmail.com>
2025-09-01 15:08:27 +0700

    routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
